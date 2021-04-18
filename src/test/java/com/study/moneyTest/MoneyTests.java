2b2e9f91764075e3f6302acb5a60375d28b178cc
lee <3bnm1128@naver.com>
2021-04-19 01:01:14 +0900

    3장. 모두를 위한 평등- Dollar

---
+++ src/test/java/com/study/moneyTest/MoneyTests.java
@@ -0,0 +1,31 @@
+package com.study.moneyTest;
+
+import com.study.tdd.demo.DemoApplication;
+import com.study.tdd.domain.Dollar;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ContextConfiguration(classes = DemoApplication.class)
+public class MoneyTests {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+    }
+
+}
+
