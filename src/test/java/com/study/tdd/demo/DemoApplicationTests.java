39ee19f38eed16803fd311ac38c76b45cabeba57
lee <3bnm1128@naver.com>
2021-04-19 00:19:59 +0900

    first commit

---
+++ src/test/java/com/study/tdd/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.study.tdd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //test
+    }
+
+}
